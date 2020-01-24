58e295e19a269bf63c9f4d755b5d29d5baf5f37a
Lucas Cabral <lcc2@cesar.org.br>
2020-01-24 07:11:07 +0000

    Add rules for some apps

--- config.def.h
+++ config.def.h
@@ -28,8 +28,11 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Gimp",             NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",          NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "qutebrowser",      NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Slack",            NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "jetbrains-studio", NULL,       NULL,       1 << 2,       0,           -1 },
 };
 
 /* layout(s) */
